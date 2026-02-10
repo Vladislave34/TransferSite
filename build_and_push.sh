ed66b5b8de3b03a9e03e77e4f6260b71d6ab4798
Ostapchuk Vlad <ostapchukvladislav77@gmail.com>
2026-02-10 17:47:45 +0200

    jenkins

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,15 +2,15 @@
 set -e  # зупиняє скрипт при помилці
 
 cd FrontendForTransfer
-docker build -t transfer-react .
-docker tag transfer-react:latest v1dkos/transfer-react:latest
-docker push v1dkos/transfer-react:latest
+docker build -t transfer-react-aws --platform linux/amd64,linux/arm64 --build-arg VITE_API_BASE_URL=https://v1dkos.itstep.click .
+docker tag transfer-react-aws:latest v1dkos/transfer-react-aws:latest
+docker push v1dkos/transfer-react-aws:latest
 echo "Done ---client---!"
 
 cd ../WebApiTransfer
-docker build -t transfer-api .
-docker tag transfer-api:latest v1dkos/transfer-api:latest
-docker push v1dkos/transfer-api:latest
+docker build -t transfer-api-aws --platform linux/amd64,linux/arm64 .
+docker tag transfer-api-aws:latest v1dkos/transfer-api-aws:latest
+docker push v1dkos/transfer-api-aws:latest
 
 echo "Done ---api---!"
 
